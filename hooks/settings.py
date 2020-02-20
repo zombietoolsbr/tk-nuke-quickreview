1f0aab30e03c29c86a66351b03635d8c625ade54
Zombie Studio <60188226+zombietoolsbr@users.noreply.github.com>
2020-02-20 17:50:28 -0300

    Update settings.py

--- hooks/settings.py
+++ hooks/settings.py
@@ -130,7 +130,7 @@ class Settings(HookBaseClass):
 
         :returns: tuple with (width, height)
         """
-        return 1280, 720
+        return 1920,1080
 
     def setup_quicktime_node(self, write_node):
         """
@@ -144,7 +144,7 @@ class Settings(HookBaseClass):
                 # http://help.thefoundry.co.uk/nuke/9.0/#appendices/appendixc/supported_file_formats.html
                 write_node["file_type"].setValue("mov64")
                 write_node["mov64_codec"].setValue("jpeg")
-                write_node["mov64_quality_max"].setValue("3")
+                write_node["mov64_quality_max"].setValue("10")
             else:
                 # the 'codec' knob name was changed to 'format' in Nuke 7
                 write_node["file_type"].setValue("ffmpeg")
@@ -154,14 +154,14 @@ class Settings(HookBaseClass):
             # that, we have to account for changes in the .mov encoding settings in the Write node.
             write_node["file_type"].setValue("mov64")
             write_node["meta_codec"].setValue("jpeg")
-            write_node["mov64_quality_max"].setValue("3")    
+            write_node["mov64_quality_max"].setValue("10")    
         else:
             write_node["file_type"].setValue("mov")
             if nuke.NUKE_VERSION_MAJOR >= 9:
                 # Nuke 9.0v1 changed the codec knob name to meta_codec and added an encoder knob
                 # (which defaults to the new mov64 encoder/decoder).                  
                 write_node["meta_codec"].setValue("jpeg")
-                write_node["mov64_quality_max"].setValue("3")
+                write_node["mov64_quality_max"].setValue("10")
             else:
                 write_node["codec"].setValue("jpeg")
             write_node["fps"].setValue(23.97599983)

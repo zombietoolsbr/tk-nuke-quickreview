011d9707040b13521a3e01f93659bca08036dff4
Zombie Studio <60188226+zombietoolsbr@users.noreply.github.com>
2020-02-21 00:09:22 -0300

    Update settings.py

--- hooks/settings.py
+++ hooks/settings.py
@@ -142,9 +142,8 @@ class Settings(HookBaseClass):
             if nuke.NUKE_VERSION_MAJOR >= 9:
                 # Nuke 9.0v1 removed ffmpeg and replaced it with the mov64 writer
                 # http://help.thefoundry.co.uk/nuke/9.0/#appendices/appendixc/supported_file_formats.html
-                write_node["file_type"].setValue("mov64")
-                write_node["mov64_codec"].setValue("jpeg")
-                write_node["mov64_quality_max"].setValue("9")
+                write_node["file_type"].setValue("mov")
+                write_node["meta_codec"].setValue(2)
             else:
                 # the 'codec' knob name was changed to 'format' in Nuke 7
                 write_node["file_type"].setValue("ffmpeg")
@@ -152,18 +151,16 @@ class Settings(HookBaseClass):
         elif nuke.NUKE_VERSION_MAJOR >= 10 and (nuke.NUKE_VERSION_MINOR > 1 or nuke.NUKE_VERSION_RELEASE > 1):
             # In Nuke 10.0v2, the dependency on the Quicktime desktop application was removed. Because of
             # that, we have to account for changes in the .mov encoding settings in the Write node.
-            write_node["file_type"].setValue("mov64")
-            write_node["meta_codec"].setValue("jpeg")
-            write_node["mov64_quality_max"].setValue("9")    
+                write_node["file_type"].setValue("mov")
+                write_node["meta_codec"].setValue(2)
         else:
             write_node["file_type"].setValue("mov")
             if nuke.NUKE_VERSION_MAJOR >= 9:
                 # Nuke 9.0v1 changed the codec knob name to meta_codec and added an encoder knob
                 # (which defaults to the new mov64 encoder/decoder).                  
-                write_node["meta_codec"].setValue("jpeg")
-                write_node["mov64_quality_max"].setValue("9")
+                write_node["meta_codec"].setValue(2)
             else:
-                write_node["codec"].setValue("jpeg")
+                write_node["codec"].setValue("mov")
             write_node["fps"].setValue(23.97599983)
 
             # note: in older versions of nuke, this settings string represents all the quicktime
